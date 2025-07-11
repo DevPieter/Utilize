2aeef4459009998a5e5f9c054d8d7db7fbe34844
Pieter <51260315+DevPieter@users.noreply.github.com>
2025-07-11 23:42:17 +0200

    🔧 Update DebugRendererMixin to conditionally import Frustum based on Minecraft version

--- src/main/java/nl/devpieter/utilize/mixins/DebugRendererMixin.java
+++ src/main/java/nl/devpieter/utilize/mixins/DebugRendererMixin.java
@@ -1,6 +1,5 @@
 package nl.devpieter.utilize.mixins;
 
-import net.minecraft.client.render.Frustum;
 import net.minecraft.client.render.VertexConsumerProvider;
 import net.minecraft.client.render.debug.DebugRenderer;
 import net.minecraft.client.util.math.MatrixStack;
@@ -11,6 +10,10 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+//#if MC>=12102
+import net.minecraft.client.render.Frustum;
+//#endif
+
 @Mixin(DebugRenderer.class)
 public class DebugRendererMixin {
 
@@ -18,7 +21,12 @@ public class DebugRendererMixin {
     private final DebugManager debugManager = DebugManager.getInstance();
 
     @Inject(at = @At("HEAD"), method = "render")
+    //#if MC>=12102
     private void onRender(MatrixStack matrices, Frustum frustum, VertexConsumerProvider.Immediate vertexConsumers, double cameraX, double cameraY, double cameraZ, CallbackInfo ci) {
+    //#else
+    //$$ public void render(MatrixStack matrices, VertexConsumerProvider.Immediate vertexConsumers, double cameraX, double cameraY, double cameraZ, CallbackInfo ci) {
+    //#endif
+
         for (DebugRenderer.Renderer renderer : this.debugManager.getRenderers()) {
             renderer.render(matrices, vertexConsumers, cameraX, cameraY, cameraZ);
         }
