3b62b2a339bbbdc43212595eb909452646a5f9e7
Pieter <51260315+DevPieter@users.noreply.github.com>
2025-07-18 12:09:11 +0200

    Version/1.0.11 (#23)
    * 🔧 Enhance null safety and add utility methods in settings and utils classes
    
    * ✏️ Mark methods as deprecated
    
    * 🐞 Add null check to AsyncRequest constructor
    
    * 🔧 Update maven-publish.yml to trigger builds on snapshot branches instead of dev
    
    * Update versioning scheme to include build number for snapshot releases (#13)
    
    * 👌 Refactor FileUtils, MathUtils, and RandomUtils for improved functionality and clarity
    
    * Align version/1.0.11 with dev (#18)
    
    * Update versioning scheme to include build number for snapshot releases
    
    * Update issue templates (#16)
    
    * Update issue templates (#17)
    
    * Align version/1.0.11 with dev (#19)
    
    * Update versioning scheme to include build number for snapshot releases
    
    * Update issue templates (#16)
    
    * Update issue templates (#17)
    
    * Align version/1.0.11 with main (#20)
    
    * Update issue templates (#16)
    
    * Update issue templates (#17)

--- src/main/java/nl/devpieter/utilize/utils/NetworkUtils.java
+++ src/main/java/nl/devpieter/utilize/utils/NetworkUtils.java
@@ -1,22 +1,23 @@
 package nl.devpieter.utilize.utils;
 
 import net.minecraft.network.packet.Packet;
+import org.jetbrains.annotations.NotNull;
 
 public class NetworkUtils {
 
-    public static void sendPacket(Packet<?> packet) {
+    public static void sendPacket(@NotNull Packet<?> packet) {
         if (!ClientUtils.hasNetworkHandler()) return;
         ClientUtils.getNetworkHandler().sendPacket(packet);
     }
 
-    public static void sendChatMessage(String message) {
+    public static void sendChatMessage(@NotNull String message) {
         if (!ClientUtils.hasNetworkHandler()) return;
 
         if (message.startsWith("/")) sendChatCommand(message);
         else ClientUtils.getNetworkHandler().sendChatMessage(message);
     }
 
-    public static void sendChatCommand(String command) {
+    public static void sendChatCommand(@NotNull String command) {
         if (!ClientUtils.hasNetworkHandler()) return;
         if (command.startsWith("/")) command = command.substring(1);
 
---
+++ src/main/java/nl/devpieter/utilize/utils/common/FileUtils.java
@@ -0,0 +1,58 @@
+package nl.devpieter.utilize.utils.common;
+
+import java.io.File;
+
+public class FileUtils {
+
+    public static boolean doesFileExist(File file) {
+        try {
+            return file.exists();
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    public static boolean doesDirectoryExist(File directory) {
+        try {
+            return directory.exists() && directory.isDirectory();
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    public static boolean tryCreateParentDirectories(File file) {
+        try {
+            File parentDir = file.getParentFile();
+            if (parentDir.exists()) return true;
+
+            return parentDir.mkdirs();
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    public static boolean tryCreateFile(File file) {
+        return tryCreateFile(file, true);
+    }
+
+    public static boolean tryCreateFile(File file, boolean createParentDirectories) {
+        try {
+            if (file.exists()) return true;
+            if (createParentDirectories && !tryCreateParentDirectories(file)) return false;
+
+            return file.createNewFile();
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    public static boolean tryDeleteFile(File file) {
+        try {
+            if (!file.exists()) return true;
+
+            return file.delete();
+        } catch (Exception e) {
+            return false;
+        }
+    }
+}
