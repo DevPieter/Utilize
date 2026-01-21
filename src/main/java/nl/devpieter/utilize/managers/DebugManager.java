f46928ecf3953eb5106d58a8870801d3782290a5
Pieter <pieter@devpieter.nl>
2026-01-21 13:35:57 +0100

    Fix DebugManager

--- src/main/java/nl/devpieter/utilize/managers/DebugManager.java
+++ src/main/java/nl/devpieter/utilize/managers/DebugManager.java
@@ -1,16 +1,17 @@
 package nl.devpieter.utilize.managers;
 
+import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.render.debug.DebugRenderer;
+import nl.devpieter.utilize.mixins.accessors.DebugRendererAccessor;
 import org.jetbrains.annotations.NotNull;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public final class DebugManager {
 
     private static final DebugManager INSTANCE = new DebugManager();
 
-    private final List<DebugRenderer.Renderer> renderers = new ArrayList<>();
+    private final MinecraftClient minecraftClient = MinecraftClient.getInstance();
 
     private DebugManager() {
     }
@@ -20,15 +21,18 @@ public final class DebugManager {
     }
 
     public void addRenderer(@NotNull DebugRenderer.Renderer renderer) {
+        List<DebugRenderer.Renderer> renderers = getAccessor().utilize$getDebugRenderers();
         if (renderers.contains(renderer)) return;
+
         renderers.add(renderer);
     }
 
     public void removeRenderer(@NotNull DebugRenderer.Renderer renderer) {
+        List<DebugRenderer.Renderer> renderers = getAccessor().utilize$getDebugRenderers();
         renderers.remove(renderer);
     }
 
-    public List<DebugRenderer.Renderer> getRenderers() {
-        return renderers;
+    private DebugRendererAccessor getAccessor() {
+        return (DebugRendererAccessor) minecraftClient.worldRenderer.debugRenderer;
     }
 }
