eb90bc5fd831d19dce4026cbe3cf53d47cb80130
Pieter <pieter@devpieter.nl>
2026-04-06 12:48:02 +0200

    Refactor TextFormatUtils to work recursively

--- src/main/java/nl/devpieter/utilize/text/formatter/ITextFormatter.java
+++ src/main/java/nl/devpieter/utilize/text/formatter/ITextFormatter.java
@@ -1,9 +1,8 @@
 package nl.devpieter.utilize.text.formatter;
 
-import net.minecraft.text.MutableText;
 import net.minecraft.text.Style;
 
 public interface ITextFormatter {
 
-    MutableText format(String content, Style baseStyle);
+    Style applyFormatting(Style parent);
 }
